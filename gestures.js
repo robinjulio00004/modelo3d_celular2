0fb6793760ce8ad3890b8525bcb1a540c2f82bcb
julio00004 <robinjulio004@gmail.com>
2025-08-21 14:59:27 -0600

    Update gestures.js

--- gestures.js
+++ gestures.js
@@ -44,6 +44,8 @@ AFRAME.registerComponent("gesture-handler", {
     if (this.isVisible) {
       this.el.object3D.rotation.y +=
         event.detail.positionChange.x * this.data.rotationFactor;
+      this.el.object3D.rotation.x +=
+        event.detail.positionChange.y;
       /*this.el.object3D.rotation.x +=
         event.detail.positionChange.y * this.data.rotationFactor;*/
     }
@@ -232,5 +234,6 @@ AFRAME.registerComponent("gesture-detector", {
     return numberNames[Math.min(touchCount, 4) - 1];
   }
 });
+
 
 
